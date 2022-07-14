d60bbbfa7756daeb550577aa2e24166f96663e89
seunghyubchoi <seunghyub.choi@gmail.com>
2022-07-13 21:43:25 -0700

    문자

--- src/VarEx3.java
+++ src/VarEx3.java
@@ -29,8 +29,8 @@ public class VarEx3 {
 		String str3 = "123";
 		String str4 = str2 + str3;
 		System.out.println(str4);
-		System.out.println("" + 7 + 7);
-		System.out.println(7 + 7 + "");
+		System.out.println("" + 7 + 7); // 77
+		System.out.println(7 + 7 + ""); // 14
 
 
 	}

a37f6e6f0f254955de86fa02f3f9cfb3261d8b53
seunghyubchoi <seunghyub.choi@gmail.com>
2022-07-14 23:28:46 -0700

    scanner

---
+++ src/ScanfEx1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ScanfEx1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		int num = scanner.nextInt();
+//		int num2 = scanner.nextInt();
+//
+//		System.out.println(num);
+//		System.out.println(num2);
+
+		String input = scanner.nextLine(); // 라인 단위로의 입력을 받음
+		int num = Integer.parseInt(input);
+		System.out.println(num);
+	}
+}
